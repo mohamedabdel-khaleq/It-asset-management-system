b243b86fb5b516843cd7527378735424e76cde94
Mohamed AbdElkhaleq <mohamedabdelkhaleq2004@gmail.com>
2026-07-16 11:16:35 +0300

    Fix employee details if conditions and update UI

--- client/it-asset-managment/src/app/features/reports/reports.ts
+++ client/it-asset-managment/src/app/features/reports/reports.ts
@@ -1,9 +1,108 @@
 import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+import { BaseChartDirective } from 'ng2-charts';
+import {
+  ChartConfiguration,
+  ChartType
+} from 'chart.js';
+
 
 @Component({
   selector: 'app-reports',
-  imports: [],
+  standalone: true,
+  imports: [
+    CommonModule,
+    BaseChartDirective
+  ],
   templateUrl: './reports.html',
-  styleUrl: './reports.css',
+  styleUrl: './reports.css'
 })
-export class Reports {}
+export class Reports {
+
+
+  // Bar Chart
+  barChartType: ChartType = 'bar';
+
+  barChartData: ChartConfiguration['data'] = {
+
+    labels: [
+      'Employees',
+      'Departments',
+      'Devices'
+    ],
+
+    datasets: [
+      {
+        data: [
+          120,
+          8,
+          350
+        ],
+        label: 'System Data'
+      }
+    ]
+
+  };
+
+
+  // Pie Chart
+
+  pieChartType: ChartType = 'pie';
+
+
+  pieChartData: ChartConfiguration['data'] = {
+
+    labels: [
+      'Available',
+      'Assigned',
+      'Maintenance',
+      'Retired'
+    ],
+
+    datasets: [
+      {
+        data:[
+          100,
+          180,
+          40,
+          30
+        ],
+        label:'Devices Status'
+      }
+    ]
+
+  };
+
+
+
+  // Doughnut Chart
+
+  doughnutChartType: ChartType = 'doughnut';
+
+
+  doughnutChartData: ChartConfiguration['data'] = {
+
+    labels:[
+      'Laptop',
+      'Desktop',
+      'Printer',
+      'Other'
+    ],
+
+    datasets:[
+      {
+        data:[
+          150,
+          120,
+          50,
+          30
+        ],
+        label:'Device Types'
+      }
+    ]
+
+  };
+
+
+}
